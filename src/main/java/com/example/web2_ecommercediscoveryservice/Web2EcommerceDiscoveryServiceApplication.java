b2a7b786a12a5a3fd34e29d731fcd2bebf44a1fc
ImKunYoung <lim8667@gmail.com>
2022-09-23 18:38:23 +0900

    init commit

---
+++ src/main/java/com/example/web2_ecommercediscoveryservice/Web2EcommerceDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.web2_ecommercediscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2EcommerceDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web2EcommerceDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web2_ecommercediscoveryservice/Web2EcommerceDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2_ecommercediscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2EcommerceDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
